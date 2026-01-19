89d280b8ebac3eaaae827582890291b71867b982
Asep Wahyudi <asepwahyudi1501@gmail.com>
2026-01-19 16:19:36 +0700

    fix: supabase config

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -5,10 +5,9 @@
 class SupabaseConfig {
   // Ganti dengan URL project Supabase Anda
   // Contoh: 'https://xxxxxxxxxxxxx.supabase.co'
-  static const String supabaseUrl = 'https://kverepckijnfymfwnqal.supabase.co';
+  static const String supabaseUrl = 'https://xxxxxxxxxxxxx.supabase.co';
 
   // Ganti dengan anon/public key dari project Supabase Anda
   // Contoh: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
-  static const String supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2ZXJlcGNraWpuZnltZnducWFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg4MDk3MTQsImV4cCI6MjA4NDM4NTcxNH0.zGNOP6KedcM2KjumNHlRyt-rp-DFp3LMUPGw9TGbLgM';
+  static const String supabaseAnonKey = 'supabase_anon_key';
 }
